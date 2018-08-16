e1211a0dfe8b110f34f4b16f1d512441add64c9d
alexvolt1 <alexvolt1@users.noreply.github.com>
2018-08-16 00:00:20 -0400

    commit

--- WCSummerCampRegistration/Controllers/HomeController.cs
+++ WCSummerCampRegistration/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WCSummerCampRegistration.Data;
 using WCSummerCampRegistration.Models;
 using WCSummerCampRegistration.Models.HomeViewModels;
@@ -13,20 +14,21 @@ namespace WCSummerCampRegistration.Controllers
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext _context;
+
         public HomeController(ApplicationDbContext context)
         {
             _context = context;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            //IndexViewModel IndexVM = new IndexViewModel()
-            //{
+            IndexViewModel IndexVM = new IndexViewModel()
+            {
+                AvailWeek = await _context.AvailWeeks.Include(m => m.Category).Include(m => m.Camp).ToListAsync(),
+                Category = _context.Categories.OrderBy(c => c.Id),
+                Camp = _context.Camps.Where(c => c.IsAvailable).ToList()
                 
-            //    Category = _context.Categories.OrderBy(c => c.Id),
-            //    Camp = _context.Camps.Where(c => c.isActive == true).ToList(),
-            //    MenuItem = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync()
-            //};
-            return View();
+            };
+            return View(IndexVM);
         }
 
         public IActionResult About()
