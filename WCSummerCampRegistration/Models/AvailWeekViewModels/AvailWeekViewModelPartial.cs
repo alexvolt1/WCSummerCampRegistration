70089e585efda8fea349b61289bf4d5742c0fa53
alex volt <alexvolt@hotmail.com>
2018-08-16 16:44:19 -0400

    commit

---
+++ WCSummerCampRegistration/Models/AvailWeekViewModels/AvailWeekViewModelPartial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WCSummerCampRegistration.Models.AvailWeekViewModels
+{
+    public class AvailWeekViewModelPartial
+    {
+
+        public IEnumerable<AvailWeek> AvailWeek { get; set; }
+        //public IEnumerable<AvailWeek.EWeekStarts> EnumerableEWeekStarts { get; set; }
+        public IEnumerable<Category> Category { get; set; }
+        public IEnumerable<Camp> Camp { get; set; }
+
+
+    }
+}
